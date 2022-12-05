1102043c18f71045024d9684e11871e70be2cee9
VigM <vmikulis@figure.com>
2022-12-05 10:48:13 -0700

    add in new package json and import values to show it working

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createHash } from "crypto";
+import { format } from "util";
+import { Writable } from "stream";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Buffer exists: {`${!!window?.Buffer}`}</div>
+        <div>crypto exists: {`${!!createHash}`}</div>
+        <div>stream exists: {`${!!Writable}`}</div>
+        <div>util exits: {`${!!format}`}</div>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Buffer } from "buffer";
+
+// If we don't have a value for Buffer (node core module) create it/polyfill it
+if (window.Buffer === undefined) window.Buffer = Buffer;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
